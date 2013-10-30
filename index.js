ff99cdffa39a320b9771012b678548304f0b9cb8
Simone Lusenti <simone@plasticpanda.com>
2013-10-30 15:38:17 +0100

    error id

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 'use strict';
 
-var util = require('util');
+var nodeutil = require('util')
+  , uuid = require('uuid')
+  ;
 
 function AppError(debugMsg, obj, originalError) {
   Error.call(this);
@@ -17,11 +19,9 @@ AppError.prototype.toString = function () {
   return this.debugMsg;
 };
 
-util.inherits(AppError, Error);
+nodeutil.inherits(AppError, Error);
 module.exports.AppError = AppError;
 
-
-module.exports.AppError = AppError;
 
 module.exports.makeAppError = function (httpStatus, name, message, debugMsg, originalError) {
   if (!debugMsg) debugMsg = message;
@@ -44,22 +44,33 @@ module.exports.makeDBError = function (originalError) {
 module.exports.middleware = function (logger) {
   
   return function (err, req, res, next) {
+    var errlogid = uuid.v4();
+    
+    logger.error('!!! Application Error !!!');
+    logger.error('> Error ID is', errlogid);
     
     if (typeof err.obj === 'object' && err.obj.http != null) {
-      logger.error('HTTP Error Handler:');
-      logger.error(err.message + '. Debug message:' + err.debugMsg);
-      logger.error('');
+      logger.error('>', err.message);
+      logger.error('>', err.debugMsg);
+      logger.error('>');
             
       if (err.originalError && err.originalError.stack) {
-        logger.error('Original error with stack trace follows:');
+        logger.error('> Original error with stack trace follows:');
         logger.error(err.originalError.stack);
+        logger.error('>');
       }
-      
-      res.render('error');
     } else {
-      logger.warn('HttBoom middleware received an Error that cannot be parsed.');
-      next(err);
+      logger.error('>', 'middleware received an error that cannot be parsed.');
+      
+      if ('production' === process.NODE_ENV) {
+        logger.error('>', nodeutil.inspect(err));
+      } else {
+        next(err);
+        return;
+      }
     }
+    
+    res.render('error', { errlogid: errlogid });
   };
 };
 
